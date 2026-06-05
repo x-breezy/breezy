12e258811128f6866d03392bca525ca1911b8ce2
Gondoue kpan Michée <kpanmichee.gondoue@viacesi.fr>
2026-06-05 16:09:33 +0200

    feat : Setting Screen

---
+++ apps/web/app/SettingsPage.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@breezy/ui/components/button"
+import SettingsScreen from "../components/SettingsScreen" 
+
+export default function Page() {
+  return (
+    <div className='flex min-h-svh p-6'>
+      <SettingsScreen name={"Grod"} username={"grod_le_goat"} />
+    </div>
+    
+
+  )
+}
